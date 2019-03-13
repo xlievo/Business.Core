d7081b7ffdebc671b3a7090636169d1c541277eb
ievo <284856919@qq.com>
2019-03-13 17:02:18 +0800

    Update BusinessMember2.cs

--- Test/WebAPI/BusinessMember2.cs
+++ Test/WebAPI/BusinessMember2.cs
@@ -1,9 +1,11 @@
 using Business;
 using Business.Attributes;
+using Business.Result;
 using Business.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using static Args;
 
@@ -34,9 +36,23 @@ public class BusinessMember : BusinessBase
     public virtual async Task<dynamic> Test001(Business.Auth.Token token, Arg<Test001> arg, [Ignore(IgnoreMode.BusinessArg)]decimal arg2 = default)
     {
         dynamic args = new System.Dynamic.ExpandoObject();
+        args.token = token;
         args.arg = arg.Out;
         args.arg2 = arg2;
         return this.ResultCreate(args);
+    }
+}
+
+public class TestAttribute : ArgumentAttribute
+{
+    public TestAttribute(int state = 111, string message = null, bool canNull = true) : base(state, message, canNull)
+    {
+    }
+
+    public override async Task<IResult> Proces(dynamic value)
+    {
+        var value2 = value + "1122";
+        return this.ResultCreate(value2);
     }
 }
 
@@ -44,6 +60,7 @@ public class Args
 {
     public struct Test001
     {
+        [Test]
         public string A { get; set; }
 
         public string B { get; set; }
